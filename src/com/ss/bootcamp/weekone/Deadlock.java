1f21c05f74de6cd7156e39321ba1ccf277b139ac
Bill Shi <jianheng.shi@smoothstack.com>
2021-03-16 11:16:08 -0400

    updated deadlock assignment

--- src/com/ss/bootcamp/weekone/Deadlock.java
+++ src/com/ss/bootcamp/weekone/Deadlock.java
@@ -4,67 +4,58 @@
 package com.ss.bootcamp.weekone;
 
 /**
- * WIP
+ * Run two threads that will deadlock
+ * Week one day four (3/11/21) assignment
  * @author Bill Shi
  *
  */
 public class Deadlock {
 	
-	static String signal1 = "Stop";
-	static String signal2 = "Stop";
+	public static Object mutex1 = new Object();
+	public static Object mutex2 = new Object();
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
-		/* each thread reads in one word */
-		/* each thread wants to print the word it read in appended to the word the other thread read in */
-				
-		Thread goSignal1 = new Thread() {
+		Thread t1 = new Thread() {
 			public void run() {
 				
-				synchronized (signal2) {
-					
-					signal1 = "Go";
-					
+				synchronized (mutex1) {					
+					System.out.println("Thread 1: Holding mutex1");
 					try {
-						while (signal2.equals("Stop")) {
-							Thread.sleep(1000);
-							System.out.println("Waiting on signal 2");
-						}
-					} catch (InterruptedException e) {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {}
+					System.out.println("Thread 1: Waiting on mutex 2");
+					synchronized (mutex2) {
+						System.out.println("Thread 2: Holding mutex1 and mutex2");
 					}
-					
-					System.out.println("Go go!");
 				}
 				
 			}
 		};
 		
-		Thread goSignal2 = new Thread() {
+		Thread t2 = new Thread() {
 			public void run() {
 				
-				synchronized (signal1) {
-					
-					signal2 = "Go";
-					
+				synchronized (mutex2) {					
+					System.out.println("Thread 2: Holding mutex2");
 					try {
-						while (signal1.equals("Stop")) {
-							Thread.sleep(1000);
-							System.out.println("Waiting on signal 1");
-						}
-					} catch (InterruptedException e) {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {}
+					System.out.println("Thread 2: Waiting on mutex 1");
+					synchronized (mutex1) {
+						System.out.println("Thread 2: Holding mutex1 and mutex2");
 					}
-					
-					System.out.println("Go go!");
 				}
 				
 			}
 		};
 		
-		goSignal1.start();
-		goSignal2.start();
+		t1.start();
+		t2.start();
+		
 	}
 
 }
